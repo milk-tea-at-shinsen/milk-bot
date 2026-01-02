25df965455804f901b8e5d5116363bb8df63eaed
milk-tea-at-shinsen <milk.tea.at.shinsen@gmail.com>
2026-01-02 11:07:59 +0900

    debug

--- bot.py
+++ bot.py
@@ -1063,7 +1063,7 @@ async def export_members(interaction: discord.Interaction):
 @app_commands.describe(minutes = "時間指定(分)", counts = "件数指定(件)")
 async def table_ocr(interaction: discord.Interaction, minutes: str = None, counts: str = None):
     await interaction.response.defer()
-    int_msg = await interaction.channel.fetch_message(interaction.message.data["id"])
+    int_msg = await interaction.channel.fetch_message(interaction.data["id"])
 
     # 返信コマンド実行時
     if int_msg.reference.resolved:
